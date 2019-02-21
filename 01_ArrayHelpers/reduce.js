4ddee9fefa2cb35cc26838528157cedc5fdba8a2
kalinchih <kalinchih@gmail.com>
2019-02-22 00:57:21 +0800

    reduce

---
+++ 01_ArrayHelpers/reduce.js
@@ -0,0 +1,45 @@
+var numbers = [10, 20, 30];
+
+var sum = numbers.reduce(function(sum, number) {
+  return sum + number;
+}, 0);
+
+console.log(sum);
+
+////
+
+var colors = [{ color: "red" }, { color: "blue" }, { color: "yellow" }];
+
+var colors1 = colors.map(function(c) {
+  return c.color;
+});
+
+console.log(colors1);
+
+var colors2 = colors.reduce(function(colors2, c) {
+  colors2.push(c.color);
+  return colors2;
+}, []);
+
+console.log(colors2);
+///////////////
+
+function balancedParens(text) {
+  return !text.split("").reduce(function(previous, char) {
+    if (previous < 0) {
+      return previous;
+    }
+    if (char === "(") {
+      return ++previous;
+    } else if (char === ")") {
+      return --previous;
+    } else {
+      return previous;
+    }
+  }, 0);
+}
+
+var isBalancedParens = balancedParens("((xx))");
+console.log(isBalancedParens);
+isBalancedParens = balancedParens(")xx(");
+console.log(isBalancedParens);
