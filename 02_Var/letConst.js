68eb8ce0fb57674cc7a11d8fbe277cab28c2faea
kalinchih <kalinchih@gmail.com>
2019-02-22 09:57:16 +0800

    enrich

--- 02_Var/letConst.js
+++ 02_Var/letConst.js
@@ -1,3 +1,10 @@
 const empNo = 123;
 let name = "Ken";
 let title = "Software Engineer";
+
+// TypeError: Assignment to constant variable.
+empNo = 456;
+console.log(empNo);
+
+title = "Senior Software Engineer";
+console.log(title);
