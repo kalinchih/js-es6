457da832cf93fd44b8ca622a57cde8a8eb3d8ddf
kalinchih <kalinchih@gmail.com>
2019-02-23 08:13:30 +0800

    template string

---
+++ 03_TemplateString/templateString.js
@@ -0,0 +1,11 @@
+function doubleMessage(number) {
+  return `Your number doubled is ${2 * number}`;
+}
+
+console.log(doubleMessage(3));
+
+function fullName(firstName, lastName) {
+  return `${firstName} ${lastName}`;
+}
+
+console.log("Tom", "Wang");
