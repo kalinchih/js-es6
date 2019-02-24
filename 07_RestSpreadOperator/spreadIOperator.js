af095000385bdd6dec859de431d6a28ba41ea736
kalinchih <kalinchih@gmail.com>
2019-02-24 20:41:48 +0800

    enrich rest & spread

--- 07_RestSpreadOperator/spreadIOperator.js
+++ 07_RestSpreadOperator/spreadIOperator.js
@@ -1,6 +1,7 @@
 const defaultColors = ["red", "blue", "yello"];
 const userFavoriteColors = ["orange", "green"];
 
+console.log(["white"].concat(defaultColors).concat(userFavoriteColors));
 const allColors = ["white", ...defaultColors, ...userFavoriteColors];
 console.log(allColors);
 // [ 'white', 'red', 'blue', 'yello', 'orange', 'green' ]
@@ -8,3 +9,9 @@ console.log(allColors);
 const allColors2 = ["white", ...defaultColors, userFavoriteColors];
 console.log(allColors2);
 // [ 'white', 'red', 'blue', 'yello', [ 'orange', 'green' ] ]
+
+function unshift(array, ...numbers) {
+  return [...numbers, ...array];
+}
+
+console.log(unshift([1, 2, 3], 4, 5, 6, 7));
