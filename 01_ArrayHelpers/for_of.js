4db980b39206e4abd4de75a14cb02fa7797b9680
kalinchih <kalinchih@gmail.com>
2019-02-27 22:55:35 +0800

    for of

---
+++ 01_ArrayHelpers/for_of.js
@@ -0,0 +1,17 @@
+const numbers = [1, 2, 3];
+const number = 5;
+for (let number of numbers) {
+  console.log(number);
+}
+console.log(number);
+
+//////////////
+const numbers2 = [1, 2, 3];
+// const number2 = 5;
+// var number2 = 5;
+number2 = 5;
+// let number2 = 5;
+for (number2 of numbers2) {
+  console.log(number2);
+}
+console.log(number2);
